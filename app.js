bfc5abb34600009667c5244ec3b4f785f0308973
Mary Ball <marusaball@yandex.ru>
2023-03-30 17:13:17 +0300

    fix/cors

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const cors = require('cors');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -17,13 +16,37 @@ const app = express();
 mongoose.set('strictQuery', true);
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/bitfilmsdb');
 
+const allowedCors = [
+  'https://domainname.maryball.nomoredomains.work',
+  'http://domainname.maryball.nomoredomains.work',
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://localhost:3001',
+  'https://localhost:3001',
+];
+
+app.use((req, res, next) => {
+  const { method } = req;
+  const { origin } = req.headers;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+  }
+  next();
+});
+
 app.use(requestLogger);
 app.use(rateLimiterUseOneHour);
 app.use(helmet());
 
 app.use(express.json());
-
-app.use(cors());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
