36c4d7f145b63e22cd040d11acfbd6cd651bc06a
Mary Ball <marusaball@yandex.ru>
2023-03-30 20:59:07 +0300

    fix/minutes

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 const Constants = require('../utils/constants');
 
 module.exports.rateLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000,
+  windowMs: 1 * 60 * 1000,
   max: 100,
   message: Constants.LIMIT_EXCEEDED,
   standardHeaders: true,
